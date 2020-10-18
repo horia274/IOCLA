05acbd1db875008ef36ef07c1957c68cc2cd4789
horia <ignat.horia.andrei@gmail.com>
2020-10-19 00:29:34 +0300

    swap with xor

--- lab_01/3-mirror/mirror.c
+++ lab_01/3-mirror/mirror.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+void swap(char *x, char *y) {
+	*x = *x ^ *y;
+	*y = *y ^ *x;
+	*x = *x ^ *y;
+}
+
 void mirror(char *s)
 {
-	char c;
 	int n = strlen(s);
 	for(int i = 0; i < (int) n / 2; i++) {
-		c = *(s + i);
-		*(s + i) = *(s + n - 1 - i);
-		*(s + n - 1 - i) = c;
+		swap(s + i, s + n - 1 - i);
 	}
 }
 
 int main()
 {
 	/* TODO: Test function */
-	char *s;//, *s2, *s3;
+	char *s;
 	s = strdup("AnaAreMere");
 	printf("%s --> ", s);
 	mirror(s);
