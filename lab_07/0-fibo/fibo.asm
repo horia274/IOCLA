5ed7531a7c7742648f05b7fa6ac55cfccfe79435
horia <ignat.horia.andrei@gmail.com>
2020-12-04 19:14:50 +0200

    lab7

---
+++ lab_07/0-fibo/fibo.asm
@@ -0,0 +1,41 @@
+%include "../utils/printf32.asm"
+
+%define NUM_FIBO 10
+
+section .text
+
+extern printf
+global main
+main:
+    mov ebp, esp
+
+    ; TODO - replace below instruction with the algorithm for the Fibonacci sequence
+    sub esp, NUM_FIBO * 4
+
+    mov ecx, NUM_FIBO
+    mov eax, 1
+    mov ebx, 1
+    push eax
+    dec ecx
+
+compute_fib:
+    push eax
+    xor edx, edx
+    add edx, eax
+    add edx, ebx
+    mov ebx, eax
+    mov eax, edx
+    loop compute_fib
+    mov ecx, NUM_FIBO
+
+print:
+    mov eax, dword [esp + (ecx - 1) * 4]
+    PRINTF32 `%d \x0`, eax
+    dec ecx
+    cmp ecx, 0
+    ja print
+
+    PRINTF32 `\n\x0`
+    mov esp, ebp
+    xor eax, eax
+    ret
